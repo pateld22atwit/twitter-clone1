b25544e4e5ce7f34d076ca927bc050f545be64dd
pateld22atwit <pateld22@wit.edu>
2021-11-29 13:32:39 -0600

    upadted try on twitter stream

--- src/index.js
+++ src/index.js
@@ -19,65 +19,16 @@ const server = http.createServer(app)
 const io = socketIo(server)
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../', 'client', 'index.css'))
+  res.sendFile(path.resolve(__dirname, '../', 'client', 'index.html'))
 })
 
-const rulesURL = 'https://api.twitter.com/2/tweets/search/stream/rules'
+const rulesURL = 'https://api.twitter.com/2/tweets/sample/stream'
 const streamURL =
-  'https://api.twitter.com/2/tweets/search/stream?tweet.fields=public_metrics&expansions=author_id'
+  'https://api.twitter.com/2/tweets/sample/stream?tweet.fields=public_metrics&expansions=author_id'
 
-const rules = [{ value: 'political' }]
 
-// Get stream rules
-async function getRules() {
-  const response = await needle('get', rulesURL, {
-    headers: {
-      Authorization: `Bearer ${TOKEN}`,
-    },
-  })
-  console.log(response.body)
-  return response.body
-}
 
-// Set stream rules
-async function setRules() {
-  const data = {
-    add: rules,
-  }
 
-  const response = await needle('post', rulesURL, data, {
-    headers: {
-      'content-type': 'application/json',
-      Authorization: `Bearer ${TOKEN}`,
-    },
-  })
-
-  return response.body
-}
-
-// Delete stream rules
-async function deleteRules(rules) {
-  if (!Array.isArray(rules.data)) {
-    return null
-  }
-
-  const ids = rules.data.map((rule) => rule.id)
-
-  const data = {
-    delete: {
-      ids: ids,
-    },
-  }
-
-  const response = await needle('post', rulesURL, data, {
-    headers: {
-      'content-type': 'application/json',
-      Authorization: `Bearer ${TOKEN}`,
-    },
-  })
-
-  return response.body
-}
 
 function streamTweets(socket) {
   const stream = needle.get(streamURL, {
@@ -100,21 +51,7 @@ function streamTweets(socket) {
 io.on('connection', async () => {
   console.log('Client connected...')
 
-  let currentRules
-
-  try {
-    //   Get all stream rules
-    currentRules = await getRules()
-
-    // Delete all stream rules
-    await deleteRules(currentRules)
-
-    // Set rules based on array above
-    await setRules()
-  } catch (error) {
-    console.error(error)
-    process.exit(1)
-  }
+  
 
   const filteredStream = streamTweets(io)
 
