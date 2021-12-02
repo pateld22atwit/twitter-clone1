8cc8d7e4eea6ff87c8cbef32ea053d3301e6b93e
githendumukiri <Barakaboston12>
2021-12-02 04:35:35 -0500

    Cleaned socket.IO code & updated twitter feed intergration

--- src/Feed.js
+++ src/Feed.js
@@ -1,33 +1,47 @@
 import React, { useState, useEffect } from "react";
 import TweetBox from "./TweetBox";
+import socketIOClient from "socket.io-client";
 import Post from "./Post";
 import "./Feed.css";
-import db from "./firebase";
-import FlipMove from "react-flip-move";
+
 
 // to-do add stream API integration here
-
 function Feed() {
-  return (
-    <div className="feed">
-      <div className="feed__header">
-        <h2>Home</h2>
-      </div>
 
-      <TweetBox />
-      <Post
-            key="this man"
-            displayName="love"
-            username="G-money"
-            verified={true}
-            text="testing"
-            //avatar={post.avatar}
-            //image={post.image}
-      />
-    </div>
-  )
+    const socket = socketIOClient("http://localhost:3000/");
+
+    socket.on('connect', () => {
+        console.log('Connected to server...')
+    })
+
+    socket.on('tweet', (tweet) => {
+        // console.log(tweet)
+        const tweetData = {
+            id: tweet.data.id,
+            text: tweet.data.text,
+            username: `@${tweet.includes.users[0].username}`,
+            displayName: `@${tweet.includes.users[0].name}`
+        }
+        return (
+            <div className="feed">
+                <div className="feed__header">
+                    <h2>Home</h2>
+                </div>
+
+                <TweetBox/>
+                <Post
+                    key={tweetData.id}
+                    displayName={tweetData.displayName}
+                    username={tweetData.username}
+                    verified={true}
+                    text={tweetData.text}
+                    //avatar={post.avatar}
+                    //image={post.image}
+                />
+            </div>
+        )
+    })
 }
-
 export default Feed;
 
 
