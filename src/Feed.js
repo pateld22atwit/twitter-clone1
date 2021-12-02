a44303d86e04039341b4f9dd31f3d3590cae4419
githendumukiri <Barakaboston12>
2021-12-02 01:26:23 -0500

    revert changes

--- src/Feed.js
+++ src/Feed.js
@@ -7,51 +7,38 @@ import FlipMove from "react-flip-move";
 
 // to-do add stream API integration here
 function Feed() {
-    /* useEffect(() => {
-      db.collection("posts").onSnapshot((snapshot) =>
-        setPosts(snapshot.docs.map((doc) => doc.data()))
-      );
-    }, []); */
-    const socket = io();
+  const [posts, setPosts] = useState([]);
 
-    const tweets = [];
+  useEffect(() => {
+    db.collection("posts").onSnapshot((snapshot) =>
+      setPosts(snapshot.docs.map((doc) => doc.data()))
+    );
+  }, []);
 
-    socket.on('connect', () => {
-        console.log('Connected to server...')
-    });
+  return (
+    <div className="feed">
+      <div className="feed__header">
+        <h2>Home</h2>
+      </div>
 
-    socket.on('tweet', (tweet) => {
-        // console.log(tweet)
-        const tweetData = {
-            id: tweet.data.id,
-            text: tweet.data.text,
-            username: `@${tweet.includes.users[0].username}`,
-            displayName: `@${tweet.includes.users[0].name}`,
+      <TweetBox />
 
-        }
-        return (
-
-            <div className="feed">
-                <div className="feed__header">
-                    <h2>Home</h2>
-                </div>
-
-                <TweetBox/>
-
-                <FlipMove>
-                    <Post
-                        key={tweetData.text}
-                        displayName={tweetData.displayName}
-                        username={tweetData.username}
-                        // verified={post.verified}
-                        text={tweetData.text}
-                        //avatar={post.avatar}
-                        //image={post.image}
-                    />
-                </FlipMove>
-            </div>
-        );
-    });
-
+      <FlipMove>
+        {posts.map((post) => (
+          <Post
+            key={post.text}
+            displayName={post.displayName}
+            username={post.username}
+            verified={post.verified}
+            text={post.text}
+            avatar={post.avatar}
+            image={post.image}
+          />
+        ))}
+      </FlipMove>
+    </div>
+  );
 }
+
+export default Feed;
 
