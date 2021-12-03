b46fd692d119162c9a133333a04f94a6fa1b2d6e
githendumukiri <Barakaboston12>
2021-12-02 19:36:40 -0500

    Use effect setup

--- src/Feed.js
+++ src/Feed.js
@@ -4,25 +4,24 @@ import socketIOClient from "socket.io-client";
 import Post from "./Post";
 import "./Feed.css";
 
-
 // to-do add stream API integration here
-function Feed() {
+function Feed (){
 
-    const socket = socketIOClient("http://localhost:3000/");
+     const [post, setPosts] = useState([]);
 
-    socket.on('connect', () => {
-        console.log('Connected to server...')
-    })
+     useEffect(() => {
+        const socket = socketIOClient('http://localhost:3000/');
 
-    socket.on('tweet', (tweet) => {
-        // console.log(tweet)
-        const tweetData = {
-            id: tweet.data.id,
-            text: tweet.data.text,
-            username: `@${tweet.includes.users[0].username}`,
-            displayName: `@${tweet.includes.users[0].name}`
-        }
-        return (
+        socket.on("connect", () => {console.log("Socket Connected");});
+        socket.on("tweets", data => {
+            console.info(data);
+            let newList = [data].concat(setPosts());
+            setPosts(newList);
+        });
+
+     }, []);
+
+      return (
             <div className="feed">
                 <div className="feed__header">
                     <h2>Home</h2>
@@ -30,18 +29,17 @@ function Feed() {
 
                 <TweetBox/>
                 <Post
-                    key={tweetData.id}
-                    displayName={tweetData.displayName}
-                    username={tweetData.username}
-                    verified={true}
-                    text={tweetData.text}
+                    key="tur"
+                    displayName="life"
+                    username="better"
+                    verified="understand"
+                    text="what is"
                     //avatar={post.avatar}
                     //image={post.image}
+
                 />
             </div>
         )
-    })
 }
 export default Feed;
-
 
--- src/index.js
+++ src/index.js
@@ -3,72 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-/*
-const http = require('http')
-const path = require('path')
-const express = require('express')
-const socketIo = require('socket.io')
-const needle = require('needle')
-const config = require('dotenv').config()
-const TOKEN = process.env.TWITTER_BEARER_TOKEN
-const PORT = process.env.PORT || 3000
 
-const app = express()
-
-const server = http.createServer(app)
-const io = socketIo(server)
-
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../', 'client', 'index.html'))
-})
-
-const rulesURL = 'https://api.twitter.com/2/tweets/sample/stream'
-const streamURL =
-  'https://api.twitter.com/2/tweets/sample/stream?tweet.fields=public_metrics&expansions=author_id'
-
-
-
-
-
-function streamTweets(socket) {
-  const stream = needle.get(streamURL, {
-    headers: {
-      Authorization: `Bearer ${TOKEN}`,
-    },
-  })
-
-  stream.on('data', (data) => {
-    try {
-      const json = JSON.parse(data)
-      console.log(json)
-      socket.emit('tweet', json)
-    } catch (error) {}
-  })
-
-  return stream
-}
-
-io.on('connection', async () => {
-  console.log('Client connected...')
-
-  
-
-  const filteredStream = streamTweets(io)
-
-  let timeout = 0
-  filteredStream.on('timeout', () => {
-    // Reconnect on error
-    console.warn('A connection error occurred. Reconnecting…')
-    setTimeout(() => {
-      timeout++
-      streamTweets(io)
-    }, 2 ** timeout)
-    streamTweets(io)
-  })
-})
-
-server.listen(PORT, () => console.log(`Listening on port ${PORT}`))
-*/
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -76,5 +11,7 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
